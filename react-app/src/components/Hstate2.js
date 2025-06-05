0528b62a01ca02e9a0fc838408c6762beeac82be
MB_SciTech <23002170110010@mail.ljku.edu.in>
2025-06-05 09:20:59 +0530

    imported required libraries

--- react-app/src/components/Hstate2.js
+++ react-app/src/components/Hstate2.js
@@ -2,6 +2,7 @@ import a1 from '../images/a1.jpg';
 import shampoo1 from '../images/shampoo1.jpg';
 import shampoo2 from '../images/shampoo2.jpg';
 import shampoo3 from '../images/shampoo3.jpg';
+import { useState } from 'react';
 
 function Hstate2(){
     var[i, setim] = useState(a1);
